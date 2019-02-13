8cb03c3b420be2bbeb2765a59de144deb18da8ed
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-02-13 15:03:42 -0600

    added HammingDist class and private variables

--- HammingDist.java
+++ HammingDist.java
@@ -3,3 +3,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+
+public class HammingDist{
+	private String city1;
+	private String city2;
+	private ArrayList<String> hammingArray = new ArrayList<String>();
+	
+	
+}
